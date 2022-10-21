5131d1d6f9ed5350e0e67719aa603f72a2feb376
Asma <asmi.mha+1@gmail.com>
2022-10-21 21:55:34 +1100

    Using UseContext

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
@@ -8,6 +8,7 @@ import {
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
+import { UserContext } from "../../contexts/user.context";
 import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
@@ -21,6 +22,7 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,8 @@ signInWithPopup,
 GoogleAuthProvider,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
@@ -77,3 +79,8 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   
     return await signInAuthUserWithEmailAndPassword(auth, email, password);
   };
+
+  export const signOutUser= async() => await signOut(auth);
+
+  export const onAuthStateChangedListener = (callback) =>
+    onAuthStateChanged(auth, callback);
